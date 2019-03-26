2529f2ec43e2769da027baed052480280a6a01d2
Cleiton Dantas dos Anjos <cleiton.dantas@outlook.com>
2019-03-26 09:53:20 -0300

    Merge Branch

---
+++ portal_front/src/app/models/dadosfaturamento.ts
@@ -0,0 +1,24 @@
+export class DadosFaturamento {
+   
+    coddadosfaturamento:number;
+    codanalise:number
+
+    cpfcnpj:String;
+    razaosocialspe:String;
+    parcela1:number;
+    notafiscal1:String;
+    mesfaturamento1:number;
+    parcela2:number;
+    notafiscal2:String;
+    mesfaturamento2:number;
+    mesfaturado:number;
+    totalrecebimentoincorporadora:number;
+    recebimentoteoricobanco:number;
+    totalrecebido:number;
+    observacao:number;
+    totalrecebidoincorporadora:number;
+    numeronotafiscal:String;
+
+
+
+}
