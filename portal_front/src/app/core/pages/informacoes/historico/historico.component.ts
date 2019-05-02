6aeafb7bd567534cf56aee31ff103ba180cf46e3
Matheus <matheus.m_c@hotmail.com>
2019-05-02 17:52:29 -0300

    funcoes do historico

--- portal_front/src/app/core/pages/informacoes/historico/historico.component.ts
+++ portal_front/src/app/core/pages/informacoes/historico/historico.component.ts
@@ -8,6 +8,9 @@ import { Fase } from 'src/app/models/fase';
 import { Sintese } from 'src/app/models/sintese';
 import { HistoricoAnalise } from 'src/app/models/HistoricoAnalise';
 import { UsersService } from 'src/app/services/users.service';
+import { HttpResponse } from '@angular/common/http';
+import { SharedService } from 'src/app/services/shared.service';
+import { Usuario } from 'src/app/models/usuario';
 
 @Component({
   selector: 'app-historico',
@@ -17,21 +20,30 @@ import { UsersService } from 'src/app/services/users.service';
 export class HistoricoComponent implements OnInit {
   comprador: Compradores = new Compradores();
   cadInfo: CadastroInformacao = new CadastroInformacao();
+  disabledSintese: boolean = true;
+  loadSpin: boolean = false;
   fid: any;
   ponto: any[];
   fases: Fase[];
   sinteses: Sintese[];
-  sintese: Sintese;
-  fase: Fase;
-  historicoAnalises: HistoricoAnalise[];
+  historicoAnalises: HistoricoAnalise[] = [];
+  historicoAnalise: HistoricoAnalise = new HistoricoAnalise();
+
 
   constructor(
      private cadastroLogicaService: CadastroLogicaService,
      private chamadaService: AnaliseChamadasService,
      private usersService: UsersService,
-     private historicoService: HistoricoService) { }
+     private historicoService: HistoricoService,
+     private sharedService: SharedService) { }
      
   ngOnInit() {
+    this.chamadaService.getDadosCadastrais('fases').subscribe(event => {
+      if (event instanceof HttpResponse) {
+        this.fases = event.body['data'];
+        console.log(this.fases)
+      }
+    })
     this.visualizarCadInfo();
 
     this.chamadaService.buscarInformacoes.subscribe(dado => {
@@ -39,54 +51,44 @@ export class HistoricoComponent implements OnInit {
       this.visualizarCadInfo();
       }
     });
-    this.getFases();
-    this.getSintese();
+
+
     this.getHistorico();  
   }
 
-  getFases(){
-    this.historicoService.getFases().subscribe(data =>{
-      this.fases =  data['data'];
-    });
-  }
-
-  getSintese(){
-    this.historicoService.getSinteses().subscribe(data => {
-      this.sinteses = data['data'];
-    })
-  }
-
   changeFases(event){
-    console.log(event.value);
-    this.historicoService.getSintesePorFase(this.fase.numfase).subscribe(data => {
-      this.sinteses = data['data'];
+    this.loadSpin = true;   
+    const numfase: Fase = event.value;
+    this.historicoService.getSintesePorFaseRequest(numfase.numfase).subscribe(event => {
+      if (event instanceof HttpResponse) {
+        this.sinteses = event.body['data'];
+        setTimeout(() => {
+          this.loadSpin = false;
+        }, 300);
+      }
     })
+    this.disabledSintese = false;
   }
 
   getHistorico(){
    this.historicoService.getHistorico(79).subscribe(data => {
         for (let i = 0; i < data['data'].length; i++) {
             console.log('Historico->'+ data['data'][i].datahistorico);
-            this.usersService.getUsuario(data['data'][i].codcadastro);                    
-
-
+            this.usersService.getUsuario(data['data'][i].codcadastro);
       }
-    
-      this.getUsuarios();
    });
    
   }
 
-  getUsuarios(){
-
-    this.usersService.getUsuario(10001).subscribe(data=>{
-      console.log(data['tada'])
-    })
-  }
-
-  salvar(){
-    console.log(this.sintese.sintese)
-    console.log(this.fase.fase)
+  salvar(data){
+    let data2: HistoricoAnalise = new HistoricoAnalise();
+    data2 = data;
+    data2.datahistorico = new Date().toDateString();
+    let user: Usuario = this.sharedService.getSessionUsuario();
+    data2.usuario = user.codUsuario;
+    this.historicoAnalises.push(data2);
+    console.log(this.historicoAnalises);
+    this.historicoAnalise = new HistoricoAnalise();
   }
 
   visualizarCadInfo() {
@@ -104,7 +106,4 @@ export class HistoricoComponent implements OnInit {
       this.cadInfo = cadInfo;
     }
   }
-
-  
-
 }
