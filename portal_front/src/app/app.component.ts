607aa00b09b1dc742f17e7e19bc8287210261611
Cleiton Dantas dos Anjos <cleiton.dantas@outlook.com>
2019-05-06 16:09:07 -0300

    Rebuild

--- portal_front/src/app/app.component.ts
+++ portal_front/src/app/app.component.ts
@@ -18,8 +18,8 @@ ngOnInit() {
   if (isDevMode()) {
     console.log('👋 Development!');
      //environment.urlpath  = window.location.origin;
-    //environment.urlpath  = 'http://10.6.5.99:8100';
-     environment.urlpath  = 'http://localhost:8100';
+       environment.urlpath  = 'http://10.6.5.99:8100';
+     //environment.urlpath  = 'http://localhost:8100';
   } else {
     console.log('💪 Production!');
     environment.urlpath  = window.location.origin;
