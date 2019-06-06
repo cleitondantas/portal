d8e7c87df721e55042e19db81c9aac998996b57a
Cleiton Dantas dos Anjos <cleiton.dantas@outlook.com>
2019-06-06 17:36:57 -0300

    Criado usuário defalt

--- src/main/java/com/montreal/portal/PortalApplication.java
+++ src/main/java/com/montreal/portal/PortalApplication.java
@@ -1,5 +1,10 @@
 package com.montreal.portal;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +14,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.montreal.portal.entity.Role;
 import com.montreal.portal.entity.Usuario;
 import com.montreal.portal.repository.UsuarioRepository;
+import com.montreal.portal.service.RoleService;
 
 @SpringBootApplication
 @EnableCaching
@@ -22,6 +29,9 @@ public class PortalApplication  implements CommandLineRunner {
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
+	@Autowired
+	private RoleService roleService;
+	
 	
     public static void main(String[] args) {
         SpringApplication.run(PortalApplication.class, args);
@@ -32,6 +42,7 @@ public class PortalApplication  implements CommandLineRunner {
     public void run(String... strings) throws Exception {
     	
     	Usuario usuario = usuarioRepository.findByLogin("admin");
+    	Iterable<Role> roles = roleService.findAll();
     	if(usuario!=null && usuario.getLogin().contentEquals("admin")) {
     		
     	}else {
@@ -48,7 +59,12 @@ public class PortalApplication  implements CommandLineRunner {
     	usuario.setCpf("00000000000");
     	usuario.setEmail("cleiton.dantas@montreal.com.br");
     	usuario.setIsAtivo(true);
-    	
+    	usuario.setTelefone("967762182");
+    	Set<Role> aSet = new HashSet<Role>();
+    	for(Role item: roles) {
+    		aSet.add(item);
+    	}
+    	usuario.setPerfis(aSet);
     	usuarioRepository.save(usuario);
     	
     	System.out.println("PASS CRIPTO"+ pass);
