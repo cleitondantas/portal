f84cc9f71741ed15cf3059d1fc592bae1d5033fd
Cleiton Dantas dos Anjos <cleiton.dantas@outlook.com>
2019-06-21 16:36:32 -0300

    Atualizado

--- src/test/java/com/montreal/portal/PortalTest.java
+++ src/test/java/com/montreal/portal/PortalTest.java
@@ -1,6 +1,8 @@
 package com.montreal.portal;
 
 public class PortalTest {
+	
+	
 
 	public static void main(String[] args) {
 		try {
@@ -13,7 +15,11 @@ public class PortalTest {
 	}
 	
 	public void test() throws InterruptedException {
-		
+		for ( int i = 0 ; i < 20 ; i ++) { 
+			 System.out.println ( i ); 
+			 } 
+		int i = 15 ;
+		System.out.println (i);
 	    
 	}
 	
